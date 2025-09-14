b5e81fabff25a642ceca6ecd173cface72a077f6
Jared Mauch <jared@puck.nether.net>
2025-09-14 01:03:04 -0400

    update

--- probe-busybox/libbb/atlas_write_response.c
+++ probe-busybox/libbb/atlas_write_response.c
@@ -29,32 +29,34 @@
 #define RESP_ADDRINFO_10	10
 
 /* External flag to indicate if JSON output is enabled */
+#ifdef CONFIG_HAVE_JSON_C
 extern int using_json;
+#else
+static int using_json = 0;
+#endif
 
 /* Response type names for JSON output */
 static const char* get_response_type_name(int type) {
-    switch (type) {
-        case RESP_PACKET: return "RESP_PACKET";
-        case RESP_DSTADDR: return "RESP_DSTADDR";
-        case RESP_SOCKNAME: return "RESP_SOCKNAME";
-        case RESP_PEERNAME: return "RESP_PEERNAME";
-        case RESP_TTL: return "RESP_TTL";
-        case RESP_TIMEOUT: return "RESP_TIMEOUT";
-        case RESP_READ_ERROR: return "RESP_READ_ERROR";
-        case RESP_LENGTH: return "RESP_LENGTH";
-        case RESP_PROTO: return "RESP_PROTO";
-        case RESP_RCVDTTL: return "RESP_RCVDTTL";
-        case RESP_RCVDTCLASS: return "RESP_RCVDTCLASS";
-        case RESP_SENDTO: return "RESP_SENDTO";
-        case RESP_CMSG: return "RESP_CMSG";
-        case RESP_DATA: return "RESP_DATA";
-        case RESP_ADDRINFO: return "RESP_ADDRINFO";
-        case RESP_ADDRINFO_SA: return "RESP_ADDRINFO_SA";
-        case RESP_RESOLVER: return "RESP_RESOLVER";
-        case RESP_N_RESOLV: return "RESP_N_RESOLV";
-        case RESP_TIMEOFDAY: return "RESP_TIMEOFDAY";
-        default: return "UNKNOWN";
-    }
+    if (type == RESP_PACKET) return "RESP_PACKET";
+    if (type == RESP_SOCKNAME) return "RESP_SOCKNAME";
+    if (type == RESP_DSTADDR) return "RESP_DSTADDR";
+    if (type == RESP_PEERNAME) return "RESP_PEERNAME";
+    if (type == RESP_TTL) return "RESP_TTL";
+    if (type == RESP_TIMEOUT) return "RESP_TIMEOUT";
+    if (type == RESP_READ_ERROR) return "RESP_READ_ERROR";
+    if (type == RESP_LENGTH) return "RESP_LENGTH";
+    if (type == RESP_PROTO) return "RESP_PROTO";
+    if (type == RESP_RCVDTTL) return "RESP_RCVDTTL";
+    if (type == RESP_RCVDTCLASS) return "RESP_RCVDTCLASS";
+    if (type == RESP_SENDTO) return "RESP_SENDTO";
+    if (type == RESP_CMSG) return "RESP_CMSG";
+    if (type == RESP_DATA) return "RESP_DATA";
+    if (type == RESP_ADDRINFO) return "RESP_ADDRINFO";
+    if (type == RESP_ADDRINFO_SA) return "RESP_ADDRINFO_SA";
+    if (type == RESP_RESOLVER) return "RESP_RESOLVER";
+    if (type == RESP_N_RESOLV) return "RESP_N_RESOLV";
+    if (type == RESP_TIMEOFDAY) return "RESP_TIMEOFDAY";
+    return "UNKNOWN";
 }
 
 /* Write JSON representation of response data */
@@ -65,62 +67,43 @@ static void write_response_json(FILE *file, int type, size_t size, void *data) {
             type, get_response_type_name(type), size);
     
     /* Handle different response types */
-    switch (type) {
-        case RESP_DSTADDR:
-        case RESP_SOCKNAME:
-        case RESP_PEERNAME:
-            if (data && size >= sizeof(struct sockaddr)) {
-                json_write_sockaddr(file, "sockaddr", (struct sockaddr*)data, size);
+    if (type == RESP_DSTADDR || type == RESP_SOCKNAME || type == RESP_PEERNAME) {
+        if (data && size >= sizeof(struct sockaddr)) {
+            json_write_sockaddr(file, "sockaddr", (struct sockaddr*)data, size);
+        }
+    } else if (type == RESP_PACKET) {
+        if (data && size > 0) {
+            json_write_packet_data(file, "packet_data", data, size);
+        }
+    } else if (type == RESP_TTL || type == RESP_RCVDTTL || type == RESP_RCVDTCLASS) {
+        if (data && size > 0) {
+            if (size == sizeof(uint8_t)) {
+                fprintf(file, ",\"value\":%d", *(uint8_t*)data);
+            } else if (size == sizeof(uint16_t)) {
+                fprintf(file, ",\"value\":%d", *(uint16_t*)data);
+            } else if (size == sizeof(uint32_t)) {
+                fprintf(file, ",\"value\":%u", *(uint32_t*)data);
             }
-            break;
-            
-        case RESP_PACKET:
-            if (data && size > 0) {
-                json_write_packet_data(file, "packet_data", data, size);
+        }
+    } else if (type == RESP_PROTO) {
+        if (data && size == sizeof(uint8_t)) {
+            fprintf(file, ",\"protocol\":%d", *(uint8_t*)data);
+        }
+    } else if (type == RESP_LENGTH) {
+        if (data && size > 0) {
+            if (size == sizeof(uint32_t)) {
+                fprintf(file, ",\"length\":%u", *(uint32_t*)data);
+            } else if (size == sizeof(uint16_t)) {
+                fprintf(file, ",\"length\":%d", *(uint16_t*)data);
             }
-            break;
-            
-        case RESP_TTL:
-        case RESP_RCVDTTL:
-        case RESP_RCVDTCLASS:
-            if (data && size > 0) {
-                if (size == sizeof(uint8_t)) {
-                    fprintf(file, ",\"value\":%d", *(uint8_t*)data);
-                } else if (size == sizeof(uint16_t)) {
-                    fprintf(file, ",\"value\":%d", *(uint16_t*)data);
-                } else if (size == sizeof(uint32_t)) {
-                    fprintf(file, ",\"value\":%u", *(uint32_t*)data);
-                }
-            }
-            break;
-            
-        case RESP_PROTO:
-            if (data && size == sizeof(uint8_t)) {
-                fprintf(file, ",\"protocol\":%d", *(uint8_t*)data);
-            }
-            break;
-            
-        case RESP_LENGTH:
-            if (data && size > 0) {
-                if (size == sizeof(uint32_t)) {
-                    fprintf(file, ",\"length\":%u", *(uint32_t*)data);
-                } else if (size == sizeof(uint16_t)) {
-                    fprintf(file, ",\"length\":%d", *(uint16_t*)data);
-                }
-            }
-            break;
-            
-        case RESP_TIMEOUT:
-        case RESP_READ_ERROR:
-            /* These are typically empty responses */
-            break;
-            
-        default:
-            /* For unknown types, include raw data as hex */
-            if (data && size > 0) {
-                json_write_packet_data(file, "raw_data", data, size);
-            }
-            break;
+        }
+    } else if (type == RESP_TIMEOUT || type == RESP_READ_ERROR) {
+        /* These are typically empty responses */
+    } else {
+        /* For unknown types, include raw data as hex */
+        if (data && size > 0) {
+            json_write_packet_data(file, "raw_data", data, size);
+        }
     }
     
     fprintf(file, "}\n");
